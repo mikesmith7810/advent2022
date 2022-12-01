65b1ccb6b0498c665bcb2d81e6d9ef5dafbf7eba
xdesign-mikesmith <mike.smith@xdesign.com>
2022-12-01 12:48:21 +0000

    initial setup

---
+++ src/test/java/com/mike/day1/application/AdventApplicationTest.java
@@ -0,0 +1,12 @@
+package com.mike.day1.application;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AdventApplicationTest {
+    @Test
+    public void shouldPassATest(){
+        assertThat(true).isTrue();
+    }
+}
